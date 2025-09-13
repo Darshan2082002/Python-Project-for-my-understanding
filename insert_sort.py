6f771d2b8d5fbac52ac160111fe3a50364ba93e8
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-13 22:11:10 +0530

    insertion sort with docstring and type hints

--- insert_sort.py
+++ insert_sort.py
@@ -1,17 +1,19 @@
 def insert(arr):
-    n = len(arr)
-    for i in range(1, n):
-        current_value = arr[i]
-        j = i - 1
-        # Shift elements of arr[0..i-1] that are greater than current_value
-        while j >= 0 and arr[j] > current_value:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = current_value
-    return arr   
-
-if __name__ == "__main__":
-    arr = [64, 25, 12, 22, 11]
-    print("Unsorted array:", arr)
-    sorted_array = insert(arr.copy())  
-    print("Sorted array:", sorted_array)
+    n=len(arr)
+    i=0
+    for i in range(n):
+        key=arr[i]
+        for j in range(i-1,-1,-1):
+            if arr[j]>key:
+                arr[j+1],arr[j]=arr[j],arr[j+1]
+            else:
+                break
+    return arr
+if __name__=="__main__":
+    import random as rd
+    arr=[]
+    arr=rd.sample(range(1,100),99)
+    print("Unsorted array is:",arr)
+    
+    ans=insert(arr)
+    print(ans)
