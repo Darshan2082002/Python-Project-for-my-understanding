9a40a3c34df82428a9635978ff92f3a742eb0ad6
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-18 13:37:35 +0530

    Logical programming

---
+++ Dict_set_1.py
@@ -0,0 +1,22 @@
+"""
+
+ Given a list of strings containing names of students and another list containing marks of corresponding students. The task is to create a dictionary to store marks of students with their names (name will be unique).
+
+Example:
+
+Input: 
+N = 5 
+names = [john, ala, ilia, sudan, mercy] 
+marks = [100, 200, 150, 80, 300]
+Output:
+ala 200 
+ilia 150 
+john 100 
+mercy 300 
+sudan 80
+Your Task:
+The task is to complete the function create_dict() which takes arr as input and creates a dictionary then returns it.
+
+Constraints:
+1 <= N <= 104
+1 <= marks <= 104"""
