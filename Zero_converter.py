97f60f4ee7147956b635352ec8a0994e40ec954c
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-18 13:56:43 +0530

    Logical problem add

--- Zero_converter.py
+++ Zero_converter.py
@@ -33,3 +33,18 @@ Constraints:
 
 """
 
+def pos(n):
+    if n==0:
+        print(0)
+    elif n>0:
+        while n>0:
+            n-=1
+            print(n,end=" ")
+            
+def neg(n):
+   if n==0:
+       print(0)
+   elif n<0:
+       while n<=0:
+           print(n,end=" ")
+           n+=1
