eea74cbc37cabb2b343ca44f73171bdb34b45417
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-21 21:59:31 +0530

    adding the function for doubly linked list file

--- python_project_1.py
+++ python_project_1.py
@@ -3,3 +3,8 @@ class Node:
         self.data=data
         self.Next=None
         self.prev=None
+def insert(data):
+def display():
+def delete(data):
+def search(data):
+def update(old_data,new_data):
