d65ac5f303b8f67571049003fa1586ebbe77cddf
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-25 10:39:55 +0530

    done coding phase1

--- find_k_largest_element.py
+++ find_k_largest_element.py
@@ -1,13 +1,19 @@
-def sort(arr):
+def sort1(arr):
     n=len(arr)
     for i in range(0,n):
         for j in range(i+1,n):
             if arr[i]>arr[j]:
                 arr[i],arr[j]=arr[j],arr[i]
+    return prin(arr)
+   
+
 def prin(arr,k):
     
-    sort(arr)
-    return arr[::-k]
+    sort1(arr)
+    
+    for i in range(-3):
+        print(arr[i])
+
 if __name__=="__main__":
     arr=[1,8,7,6,4,2]
-    print(prin(arr,3))
+    print(prin(arr, 3))
