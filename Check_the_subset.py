773601797e835baa1dc70a0b0c9453d590a5dc1e
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-25 20:14:38 +0530

    Check worked on the hash table

--- Check_the_subset.py
+++ Check_the_subset.py
@@ -1,1 +1,16 @@
 #using the hastable
+def hasha(a,b):
+    n=set(a)
+    for nums in b:
+        if nums not in n:
+            return False
+    return True
+
+if __name__=="__main__":
+  a = [11, 1, 13, 21, 3, 7]
+  b = [11, 3, 7, 1,2]
+
+  if hasha(a, b):
+      print("true")
+  else:
+      print("false")
