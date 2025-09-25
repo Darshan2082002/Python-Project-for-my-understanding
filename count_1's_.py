6d48062b507cdda9187ae1b78ecf4c868ca3e161
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-25 10:21:05 +0530

    Count problem solved

--- count_1's_.py
+++ count_1's_.py
@@ -1,1 +1,15 @@
 def count(arr):
+    n=len(arr)
+    count=0
+    for i in range(0,n):
+        
+        if arr[i]==1:
+            count=count+1
+        else:
+            continue
+    return count
+
+if __name__=="__main__":
+    arr=[1,1,0,0,0,1]
+    print(count(arr))
+    
