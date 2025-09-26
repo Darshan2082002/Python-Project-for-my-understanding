f6e203b39f71410bf0bb89a6e0e32ccf5dee52db
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-26 22:12:18 +0530

    Hashing for FizzBuzz done

--- Fizzbuzz using hash map.py
+++ Fizzbuzz using hash map.py
@@ -1,2 +1,21 @@
 def fizz(a):
+    res=[]
+    mp={3:"Fizz",5:"Buzz"}
+    divisor=[3,5]
+    for i in range(1,n+1):
+        s=""
+        for d in divisor:
+            if i%d==0:
+                s+=mp[d]
+        if not s:
+            s+=str(i)
+        res.append(s)
+    return res
+
+if __name__ == "__main__":
+    n = 20
+    res = fizz(n)
+
+    for s in res:
+        print(s, end=" ")
     
