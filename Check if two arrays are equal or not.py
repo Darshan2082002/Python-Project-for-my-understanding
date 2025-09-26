7fa2a220736e56be76afe01c513e167493b50f23
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-26 11:25:51 +0530

    Work are progress

--- Check if two arrays are equal or not.py
+++ Check if two arrays are equal or not.py
@@ -3,4 +3,14 @@ def equal(a,b):
     if n!=m:
         return False
     mp{}
-    
+    for num in a:
+        mp[num]=mp.get(num,0)+1
+    for num in b:
+        if num not in mp:
+            return False
+        if mp[num]==0:
+            return False
+        else:
+            mp[num]-=1
+    return True
+
