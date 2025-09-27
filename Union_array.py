db251cab03d84cf1d3fc6dfa9aca437c4a66d2db
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-27 21:41:11 +0530

    Planning use set for this

--- Union_array.py
+++ Union_array.py
@@ -1,11 +1,3 @@
 def union(a,b):
-    n,m=len(a),len(b)
-    if n==0 and m ==0:
-        return 0
-    mp={}
-    for i in range(n):
-        mp[a[i]]=i
-        
-    for mp in b:
-        
+    
         
