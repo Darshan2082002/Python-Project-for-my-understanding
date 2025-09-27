8e565bb98feaf51b2b175045ea21030a23765dd4
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-27 21:44:19 +0530

    doing the changes

--- Union_array.py
+++ Union_array.py
@@ -1,3 +1,14 @@
 def union(a,b):
-    
+    st=set()
+    for i in range(len(a)):
+        st.add(a[i])
+        
+    for j in range(len(b)):
+        st.add(b[i])
+        
+    res=[]
+    for it in st:
+        res.append(it)
+        
+        
         
