892f1c38e818a0749c562c33b56b676f5d87f256
Darshan2082002 <darshanbalu20@gmail.com>
2025-12-28 16:32:25 +0530

    Numpy 1 learning files

--- DSA_Preparation_Folder/Python_numpy/numpy_1.py
+++ DSA_Preparation_Folder/Python_numpy/numpy_1.py
@@ -1,8 +1,13 @@
+
 import numpy as np 
-a= np.arange(15).reshape(3,5)
-print(a)
+from numpy import random
+a= np.random.randint(15, size=(3,5))
+print(a.mean)
 print(a.shape)
 print(a.ndim)
 print(a.dtype)
 print(a.size)
 print(type(a))     
+
+for i in range(len(dir(np))):
+    print(dir(np)[i])
