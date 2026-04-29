1645d3d72722fd982688a3263e8bf31f6c315f36
Darshan2082002 <baludarshan20@gmail.com>
2026-04-29 20:12:05 +0530

    CLi program

---
+++ Python_code_practice/Cli_expensive _tracker.py
@@ -0,0 +1,18 @@
+import json 
+from datetime import datetime 
+import os
+from textwrap import indent
+
+from numpy import save 
+
+
+Data='expensive.json'
+
+def load_data():
+    if not os.path.exists(Data):
+        return []
+    with open(Data,"r") as file:
+        return json.load(file)
+def save_expensive(exp):
+    with open(Data,"w") as file:
+        json.dump(exp,file,indent=4)
