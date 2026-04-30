779047fbf60135f01db6a49a0317d2c101860cc4
Darshan2082002 <baludarshan20@gmail.com>
2026-04-30 09:23:33 +0530

    word guessing program

--- Python_code_practice/Word_guessing_game.py
+++ Python_code_practice/Word_guessing_game.py
@@ -1,1 +1,24 @@
 import random as rd
+
+def word():
+    words=["innovation", "strategy", "adventure", "nebula", "quantum"]
+    a=rd.choice(words)
+    count=0
+    print("Word Guessing Game!!!")
+    while True:
+        try:
+            n=str(input("Enter thw Word !!!!"))
+            count+=1
+        except:
+            print("Try again ")
+            continue
+        if n==a:
+            print(" Greate you found it ")
+            break
+        
+        else:
+            print('Try again !!!')
+
+
+
+word()
